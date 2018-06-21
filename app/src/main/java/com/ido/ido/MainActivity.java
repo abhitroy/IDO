59096080afd88d4f1f4482bda991aea42298de89
Abhit Roy <abhitroy.0312@gmail.com>
2018-06-21 17:37:20 +0530

    new edit profile

--- app/src/main/java/com/ido/ido/MainActivity.java
+++ app/src/main/java/com/ido/ido/MainActivity.java
@@ -3,13 +3,27 @@ package com.ido.ido;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button tst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tst=(Button)findViewById(R.id.test);
+        tst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,edit_profilex.class
+                ));
+                finish();
+            }
+        });
+
+
     }
     @Override
     public void onBackPressed() {
